754c4bfbfdb8a28c0be09b29bf2f9438b250e724
Kevin Gilds <kevingilds@gmail.com>
2024-11-10 18:06:05 -0500

    Clean up script to get actual totals for the projects

--- import-data.R
+++ import-data.R
@@ -5,14 +5,14 @@ library(clockify)
 library(gt)
 
 
-
+### set the api keys
 CLOCKIFY_API_KEY = Sys.getenv("Clockify")
 
 clockify::set_api_key(CLOCKIFY_API_KEY)
 
 
 
-
+#### Deal with Dates
 current_date <- Sys.Date()
 
 current_date
@@ -21,6 +21,7 @@ current_date_adj <- current_date -7
 
 current_date_adj
 
+###Get last week
 last_week <- lubridate::week(current_date)
 
 
@@ -29,63 +30,31 @@ last_week
 
 
 
-
+### List Projects
 pro <- projects()
 
 pro
 
 
-
+### Data Wrangle
 weekly_review <- time_entries(start=current_date_adj, concise = FALSE) %>%
   dplyr::select(-1, -2, -3, -8) %>%
   dplyr::mutate(week_num =lubridate::week(time_start)) %>%
   dplyr::left_join(pro, weekly_review, by="project_id") %>%
   dplyr::select(project_name, billable.x, duration, week_num) %>%
   dplyr::rename(billable = billable.x) %>% 
-  dplyr::rename(project = project_name)
+  dplyr::rename(project = project_name) 
 
 weekly_review
 names(weekly_review)
 
-
+### Summarize the data
 weekly_review <- weekly_review %>%
   dplyr::filter(week_num == last_week) %>%
-  group_by(project)
+  dplyr::group_by(project) |> 
+  summarise(total = sum(duration))
+
 
 weekly_review
 
 readr::write_csv(weekly_review, "weekly-review.csv")
-
-weekly_review_tbl <- weekly_review %>%
-  gt::gt()
-
-weekly_review_tbl
-
-
-sum_weekly <- weekly_review %>%
-  group_by(project) %>%
-  dplyr::summarise(total = sum(duration))
-
-sum_weekly
-
-
-sum_weekly_tbl <- sum_weekly %>%
-  gt::gt()
-
-
-sum_weekly_tbl
-
-
-
-avg_weekly <- weekly_review %>%
-  group_by(project) %>%
-  dplyr::summarise(total = mean(duration))
-
-avg_weekly
-
-avg_weekly_tbl <- avg_weekly %>%
-  gt::gt()
-
-avg_weekly_tbl
-
-
